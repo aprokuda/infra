0cf11fdc310e4b14dc3a59cdd487221d4948e9da
Alexandr Prokuda <al.prokuda@gmail.com>
2022-04-28 18:03:00 +0600

    *

--- M-Lombard.sh
+++ M-Lombard.sh
@@ -7,13 +7,13 @@ DateToday=$(date "+%d.%m.%Y")
 #cd ./deploy-version
 
 echo "2. Get version number"
-cd ./deploy-version/Get-folder-M-Lombard/ios
+cd ./deploy-version/Get-folder-M-Online/ios
 rm -r result_from_testflight
 rm -r result_build
 fastlane run latest_testflight_build_number >> result_from_testflight
 grep build: result_from_testflight >> result_build
 buildNum=$(tail -c4 result_build)
-let "buildNum += 2"
+let "buildNum += 1"
 cd ../..
 
 
